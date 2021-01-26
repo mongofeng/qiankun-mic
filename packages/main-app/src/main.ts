574f3795911aacd71a3894a51d3dcfb8d8d0a221
yangpu <yangpu@xiao100.com>
2021-01-26 10:39:53 +0800

    feat: 添加乾坤和子应用

---
+++ packages/main-app/src/main.ts
@@ -0,0 +1,21 @@
+import 'zone.js'; // for angular subapp
+import { registerMicroApps, runAfterFirstMounted, start } from 'qiankun';
+registerMicroApps([
+  {
+    name: 'react app', // app name registered
+    entry: '//localhost:8000',
+    container: '#sub',
+    activeRule: '/app1',
+  },
+  {
+    name: 'ng app',
+    entry: '//localhost:4203',
+    container: '#sub',
+    activeRule: '/ng-app',
+  },
+]);
+start()
+
+runAfterFirstMounted(() => {
+  console.log('[MainApp] first app mounted');
+});
