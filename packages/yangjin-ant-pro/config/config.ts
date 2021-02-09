2814af279dfc50a05a6c96a8f8a98df2d13023e8
yangpu <yangpu@xiao100.com>
2021-02-09 16:21:12 +0800

    feat: 添加antdpro

---
+++ packages/yangjin-ant-pro/config/config.ts
@@ -0,0 +1,66 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+import defaultSettings from './defaultSettings';
+import proxy from './proxy';
+import routes from './routes';
+
+const { REACT_APP_ENV, PORT } = process.env;
+
+
+export default defineConfig({
+  hash: true, // 配置是否让生成的文件包含 hash 后缀，通常用于增量发布和避免浏览器加载缓存。
+  history: { type: 'hash' }, // 路由模式
+  base: '/yangjin-pro/', // 路由的的目录
+  publicPath: process.env.NODE_ENV === 'production' ? '/app2/' : `//localhost:${PORT}/`, // 打包目录
+  antd: {},
+
+  qiankun: {
+    slave: {}
+  },
+  dva: {
+    hmr: true,
+  },
+  layout: {
+    name: 'Ant Design Pro',
+    locale: true,
+    siderWidth: 208,
+    ...defaultSettings,
+  },
+  locale: {
+    // default zh-CN
+    default: 'zh-CN',
+    antd: true,
+    // default true, when it is true, will use `navigator.language` overwrite default
+    baseNavigator: true,
+  },
+  dynamicImport: {
+    loading: '@ant-design/pro-layout/es/PageLoading',
+  },
+  targets: {
+    ie: 11,
+  },
+  // umi routes: https://umijs.org/docs/routing
+  routes,
+  // Theme for antd: https://ant.design/docs/react/customize-theme-cn
+  theme: {
+    'primary-color': defaultSettings.primaryColor,
+  },
+  esbuild: {},
+  title: false,
+  ignoreMomentLocale: true,
+  proxy: proxy[REACT_APP_ENV || 'dev'],
+  manifest: {
+    basePath: '/',
+  },
+  // https://github.com/zthxxx/react-dev-inspector
+  plugins: ['react-dev-inspector/plugins/umi/react-inspector'],
+  inspectorConfig: {
+    // loader options type and docs see below
+    exclude: [],
+    babelPlugins: [],
+    babelOptions: {},
+  },
+  resolve: {
+    includes: ['src/components'],
+  },
+});
