9e49bc0d8bbe47ddfbd7847b62b8ef28cee56fd1
yangpu <yangpu@xiao100.com>
2021-02-09 16:57:44 +0800

    feat: 修复默认的配置

--- packages/yangjin-pro/config/defaultSettings.ts
+++ packages/yangjin-pro/config/defaultSettings.ts
@@ -4,18 +4,26 @@ const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
 } = {
-  navTheme: 'light',
+  navTheme: 'dark',
   // 拂晓蓝
   primaryColor: '#1890ff',
-  layout: 'mix',
+  layout: 'side',
   contentWidth: 'Fluid',
-  fixedHeader: false,
+  fixedHeader: true,
   fixSiderbar: true,
+
   colorWeak: false,
   title: 'Ant Design Pro',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
+  "menu": {
+    "locale": true
+  },
+  "headerHeight": 64,
+  "splitMenus": false
+    
+  
 };
 
 export default Settings;
